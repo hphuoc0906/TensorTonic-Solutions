35978c46db55d86731a95b23dffab2a4c0762955
PHAN HOÀNG PHƯỚC <120120929+hphuoc0906@users.noreply.github.com>
2026-02-06 01:02:09 +0700

    Add solution for maxpool-forward

---
+++ maxpool-forward/maxpool-forward.py
@@ -0,0 +1,22 @@
+def maxpool_forward(X, pool_size, stride):
+    """
+    Compute the forward pass of 2D max pooling.
+    """
+
+    n_row = len(X)
+    n_col = len(X[0])
+
+    lst = []
+    for row in range(0, n_row - pool_size + 1, stride):
+        new_row = []
+        for col in range(0, n_col - pool_size + 1, stride):
+            val = -10**10
+            for i in range(row, row + pool_size):
+                for j in range(col, col + pool_size):
+                    val = max(val, X[i][j])
+            new_row.append(val)
+        lst.append(new_row)
+    
+    return lst
+
+    return lst
